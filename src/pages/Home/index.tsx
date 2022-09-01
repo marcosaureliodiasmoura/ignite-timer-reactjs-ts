4a49876fe63ae5384de04adbb5dc9196e0ca15f0
marcosaureliodiasmoura <marcos_moura@live.com>
2022-09-01 17:54:53 -0300

    reset form and include defaultValues with react-hook-forms

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -29,7 +29,7 @@ interface NewCycleFormData {
 // type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
-  const { register, handleSubmit, watch, formState } =
+  const { register, handleSubmit, watch, formState, reset } =
     useForm<NewCycleFormData>({
       resolver: zodResolver(newCycleFormValidationSchema),
       defaultValues: {
@@ -40,6 +40,7 @@ export function Home() {
 
   function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data)
+    reset()
   }
 
   console.log(formState.errors)
