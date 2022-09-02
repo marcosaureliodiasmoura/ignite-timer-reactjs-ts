78fdfdc6966b5eb292354394801590d8f505c6c2
marcosaureliodiasmoura <marcos_moura@live.com>
2022-09-02 17:29:42 -0300

    fix: convert pages Home and Countdown for contextAPI

---
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,64 @@
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
+import { FormContainer, TaskInput, MinuteAmountInput } from './styles'
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(1, 'O ciclo precisa ser de no mínimo de 5 minutos')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
+})
+
+interface NewCycleFormData {
+  task: string
+  minutesAmount: number
+}
+
+// type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
+export function NewCycleForm() {
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
+
+  return (
+    <FormContainer>
+      <label htmlFor="task">Vou trabalhar em</label>
+      <TaskInput
+        id="task"
+        list="task-suggestions"
+        placeholder="Dê um nome para o seu projeto"
+        disabled={!!activeCycle}
+        {...register('task')}
+      />
+
+      <datalist id="task-suggestions">
+        <option value="projeto 1" />
+        <option value="projeto 2" />
+        <option value="projeto 3" />
+        <option value="projeto  4" />
+      </datalist>
+
+      <label htmlFor="minutesAmount">durante</label>
+      <MinuteAmountInput
+        type="number"
+        id="minutesAmount"
+        placeholder="00"
+        step={5}
+        min={1}
+        max={60}
+        disabled={!!activeCycle}
+        {...register('minutesAmount', { valueAsNumber: true })}
+      />
+
+      <span>minutos.</span>
+    </FormContainer>
+  )
+}
